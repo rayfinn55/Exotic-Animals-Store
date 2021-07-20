8829e2cb6133e7904750f159a0c92347c796c019
Rayvon Finney Pinkston <rayfinn55@gmail.com>
2021-07-20 02:33:09 -0400

    seed.sql values

--- back-end/db/seed.sql
+++ back-end/db/seed.sql
@@ -7,25 +7,11 @@ VALUES
 ('Fennec Fox', 'Mammal', 'Africa & Western Asia', 'The fennec fox (Vulpes zerda) is a small crepuscular fox native to the Sahara Desert, the Sinai Peninsula in Egypt, and the Arava and Negev deserts in Israel. Its most distinctive feature is its unusually large ears.', '4,500', 'true' ),
 ('Red Hand Tamarin', 'Mamal', 'Guyana & South America', 'The red-handed tamarin (Saguinus midas), also known as the golden-handed tamarin or Midas tamarin, is a New World monkey belonging to the family Callitrichidae.', '4,500', 'false'),
 ('Albino Suriname Red Tail Boa', 'Reptile', 'A sought-after reptile among snake lovers, the Suriname red tail boa is both rare and beautiful. This snake is technically not a separate species (just like the Colombian red tail). It’s part of the boa constrictor family','15,000', 'true' ),
-(),
-(),
-(),
-(),
-(),
-(),
-(),
-();
-
-
-
-
-
-
-INSERT INTO test (name) VALUES
-('Monday'),
-('Tuesday'),
-('Wednesday'),
-('Thursday'),
-('Friday'),
-('Saturday'),
-('Sunday');
+('Chinchilla', 'Mammal', 'Andes Mountain', 'Chinchillas are either of two species (Chinchilla chinchilla and Chinchilla lanigera) of crepuscular rodents of the parvorder Caviomorpha.', '650', 'false'),
+('Bengal Cats', 'Mammaml','United States', 'The Bengal cat is a domesticated cat breed created from hybrids of domestic cats, especially the spotted Egyptian Mau, with the Asian leopard cat (Prionailurus bengalensis).', '5,200', 'true'),
+('Peppermint Angelfish', 'Actinopterygii (Pomacanthidae)', 'Eastern central Pacific Ocean', 'The peppermint angelfish has a deep, oval shaped body which has five white vertical bars on a reddish-orange background, these bars extending onto the fins, has alternating vertical reddish orange and white bars on the body, head and fins.', '30,000', 'false'),
+('Xingu River Ray', 'Chondrichthyes', 'Xingu River basin', 'The Xingu river ray is a venomous stingray that contains venom localized at its dentine spine in its tail.', '100,000','false'),
+('Leafy Sea Dragon', 'Syngnathidae,', 'Southern & Western Australia', `Much like the seahorse, the leafy seadragon's name is derived from its resemblance to another creature (in this case, the mythical dragon)`, '15,000', 'true'),
+('Australian Flathead Perch', 'Actinopterygii (Epinephelinae)', 'Australia', 'The rainfordia has an overall dark orange colour broken by six black-margined lilac stripes along each flank, the background orange colour is darkest on the centre of the flanks, fading to yellow on the back and belly.', '5,000','true'),
+('Kinkajou', 'Mammal', 'Central America & South America', 'They are playful, generally quiet, docile, and have little odor. However, they can occasionally be aggressive. Kinkajous dislike sudden movements, noise, and being awake during the day.', '4,000', 'true'),
+('Frilled Dragon', 'Reptile', 'Northern Australia and southern New Guinea', 'The frill-necked lizard is a relatively large member of the agamid family, growing up to 85 cm (2.79 ft). It is capable of bipedal locomotion and has been described as regularly moving in this manner with a purposeful stride at times by naturalists', '500', 'true');
