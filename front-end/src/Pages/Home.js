c86ed496d5122822d27be5c7f6ce15b0eb232820
Ivan Castillo <total180training@gmail.com>
2021-07-22 15:25:04 -0400

    Set up all required Pages. Need to finish Create and Update function

--- front-end/src/Pages/Home.js
+++ front-end/src/Pages/Home.js
@@ -8,6 +8,7 @@ export default function Home() {
         <div>
             <h1>Welcome to the Exotic Animals Shop</h1>
             <h2>We are here for all of your exotic animals needs</h2>
+            <p>In our shop (much like in life) you can choose your best friend!</p>
             <h3>This can be your new BFF on YOUR couch!</h3>
             <img src={Giraffe} alt="Adorable Giraffe" />
         </div>
