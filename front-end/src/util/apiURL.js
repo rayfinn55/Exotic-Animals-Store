ce0495ef0df9d544a20d9fed7a79bce1b55ba7fe
Rayvon Finney Pinkston <rayfinn55@gmail.com>
2021-07-23 16:03:35 -0400

    fixed api address

--- front-end/src/util/apiURL.js
+++ front-end/src/util/apiURL.js
@@ -1,7 +1,7 @@
 export const apiURL = () => {
   return window.location.hostname === "localhost"
     ? "http://localhost:3333"
-    : "https://agile-basin-65767.herokuapp.com/";
+    : "https://agile-basin-65767.herokuapp.com";
 };
 
 
