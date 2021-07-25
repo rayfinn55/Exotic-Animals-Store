63beca43a22d2b16f653c95932bc6ad7d9a9f4aa
Ivan Castillo <total180training@gmail.com>
2021-07-25 17:14:53 -0400

    Added Card.Img responsiveness

--- front-end/src/Components/NavBar.js
+++ front-end/src/Components/NavBar.js
@@ -1,14 +1,20 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { Navbar, Container, Nav } from 'react-bootstrap'
 
 
 export default function NavBar() {
 
     return (
-        <nav className='navbar navbar-light bg-light'>
-            <Link to='/'>Home</Link>
-            <Link to='/pets'>Pets</Link>
-            <Link to='/pets/new'>New</Link>
-        </nav>
+        <Navbar>
+            <Container>
+                <Nav>
+                    
+                        <Link to='/'>Home</Link>
+                        <Link to='/pets'>Pets</Link>
+                        <Link to='/pets/new'>New</Link>
+                </Nav>
+            </Container>
+        </Navbar>
     )
 }
