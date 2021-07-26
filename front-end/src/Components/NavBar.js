6abc3a3b61ff744fc76a985c812c7b736da3ddca
Ivan Castillo <total180training@gmail.com>
2021-07-26 05:57:12 -0400

    Refactored Navbar with react-bootstrap, not done styling

--- front-end/src/Components/NavBar.js
+++ front-end/src/Components/NavBar.js
@@ -7,12 +7,13 @@ import Home from '../Pages/Home'
 export default function NavBar() {
 
     return (
-        <Navbar bg='light' expand='md'>
+        <Navbar bg='warning' expand='md'>
             <Container>
+            <Navbar.Brand as={Link} to='/'>My New Best Friend</Navbar.Brand>
                     <Nav>
-                        <Link to='/'>Home</Link>
-                        <Link to='/pets'>Pets</Link>
-                        <Link to='/pets/new'>New</Link>
+                        <Nav.Link as={Link} to='/'>Home</Nav.Link>
+                        <Nav.Link as={Link} to='/pets'>Pets</Nav.Link>
+                        <Nav.Link as={Link} to='/pets/new'>New</Nav.Link>
                     </Nav>
             </Container>
         </Navbar>
