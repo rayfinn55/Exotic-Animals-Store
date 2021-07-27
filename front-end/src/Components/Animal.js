fa12224ba8d0e1be8a5c2bf4c00678a144df8163
EsayH <ez@EsayHs-Mac-mini.local>
2021-07-27 09:32:36 -0400

    center items

--- front-end/src/Components/Animal.js
+++ front-end/src/Components/Animal.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import CardGroup from 'react-bootstrap/CardGroup';
+import Container from 'react-bootstrap/Container';
 import { Link } from 'react-router-dom';
 import placeholder from '../Images/placeholder.png'
 
@@ -15,31 +16,43 @@ function Pets({ pets, loading }) {
 	}
 
 	return (
-		<CardGroup>
-			{pets.map((pet) => (
-				<div key={pet.id} className='col-md-4 mb-5'>
-					<Card className='mb-5' style={{ width: '19rem' }}>
-						<Card.Title style={{ textAlign: 'center'}}>{pet.animal_name}</Card.Title>
-						<p style={{ textAlign: 'center'}}>{pet.class}</p>
-						<Card.Img variant='top' style={{height: '100%'}} src={pet.img} onError={handleBrokenImg} alt={`A cool ${pet.animal_name}`} />
-						<Card.Text style={{ textAlign: 'center'}}>
-							<strong>Location:</strong> {pet.location}
-						</Card.Text>
-						<Card.Text style={{ textAlign: 'center' }}>
-							<strong>Cost:</strong> ${pet.price.toLocaleString()}
-						</Card.Text>
-						{
-						pet.stock ? 
-						<Card.Text style={{ textAlign: 'center'}}><strong>Availability:</strong>In Stock</Card.Text> : 
-						<Card.Text style={{ textAlign: 'center'}}><strong>Availability:</strong>Out of Stock</Card.Text>
-						}
-						<Link to={`pets/${pet.id}`} style={{ textAlign: 'center'}}>
-							<Button variant='warning'>Details</Button>
-						</Link>
-					</Card>
-				</div>
-			))}
-        </CardGroup>
+		<Container>
+			<CardGroup>
+				{pets.map((pet) => (
+					<div key={pet.id} className='col-md-4 mb-5'>
+						<Card className='mb-5' style={{ width: '19rem' }}>
+							<Card.Title style={{ textAlign: 'center' }}>{pet.animal_name}</Card.Title>
+							<p style={{ textAlign: 'center' }}>{pet.class}</p>
+							<Card.Img
+								variant='top'
+								style={{ height: '100%' }}
+								src={pet.img}
+								onError={handleBrokenImg}
+								alt={`A cool ${pet.animal_name}`}
+							/>
+							<Card.Text style={{ textAlign: 'center' }}>
+								<strong>Location:</strong> {pet.location}
+							</Card.Text>
+							<Card.Text style={{ textAlign: 'center' }}>
+								<strong>Cost:</strong> ${pet.price.toLocaleString()}
+							</Card.Text>
+							{pet.stock ? (
+								<Card.Text style={{ textAlign: 'center' }}>
+									<strong>Availability:</strong>In Stock
+								</Card.Text>
+							) : (
+								<Card.Text style={{ textAlign: 'center' }}>
+									<strong>Availability:</strong>Out of Stock
+								</Card.Text>
+							)}
+							<Link to={`pets/${pet.id}`} style={{ textAlign: 'center' }}>
+								<Button variant='warning'>Details</Button>
+							</Link>
+						</Card>
+					</div>
+				))}
+			</CardGroup>
+		</Container>
 	);
 }
 
